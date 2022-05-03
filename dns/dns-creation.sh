a8c7f9c741994dbb74cba83deb66b0d664084b11
José Daniel <jdrayman@hotmail.com>
2022-05-03 14:26:14 +0200

    Update dns-creation.sh

--- dns/dns-creation.sh
+++ dns/dns-creation.sh
@@ -1,6 +1,5 @@
 sudo apt update
 sudo apt -y upgrade
-sudo apt install -y ufw
 sudo apt install -y bind9 bind9utils bind9-doc dnsutils
 sudo ufw allow bind9
 sudo cat ./forward-zone.txt >> /etc/bind/named.conf.local
@@ -12,4 +11,5 @@ sudo touch /etc/bind/forward.miproyecto.local
 sudo touch /etc/bind/reverse.miproyecto.local
 sudo cat ./forward-file.txt >> /etc/bind/forward.miproyecto.local
 sudo cat ./reverse-file.txt >> /etc/bind/reverse.miproyecto.local
+sudo apt install -y ufw
 sudo systemctl restart bind9
